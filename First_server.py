44d7260b6c77926b8007135b6c282871af5fa3cf
nimish jindal <nimish.jindal@yahoo.com>
2019-02-18 18:58:59 +0530

    added camera click basic code using python

--- First_server.py
+++ First_server.py
@@ -1,13 +1,26 @@
 # flask_ngrok_example.py
 from flask import Flask
 from flask_ngrok import run_with_ngrok
+import picamera
+
 
 app = Flask(__name__)
 run_with_ngrok(app)  # Start ngrok when app is run
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+		
+	# Setup the camera such that it closes
+	# when we are done with it.
+	print("About to take a picture.")
+	with picamera.PiCamera() as camera:
+		camera.resolution = (1280,720)
+		filename = "/home/pi/Desktop/newimage.jpg"
+		camera.capture(filename)
+		
+	print("Picture taken.")
+	
+	return send_file(filename, mimetype='image/jpg')
 
 if __name__ == '__main__':
     app.run()
