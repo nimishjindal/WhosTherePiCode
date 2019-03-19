ab8b07b43baa99a78a707148c343a0a0ba941771
nimish jindal <nimish.jindal@yahoo.com>
2019-03-19 13:46:54 +0530

    test servo working

--- servo.py
+++ servo.py
@@ -3,21 +3,27 @@ from time import sleep
 
 pinNo = 2
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pinNo,GPIO.OUT)
-pwm=GPIO.PWM(pinNo, 50)
-pwm.start(0)
-SetAngle(90) 
-pwm.stop()
-GPIO.cleanup()
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
 	GPIO.output(pinNo, True)
 	pwm.ChangeDutyCycle(duty)
-	sleep(1)
+	sleep(7)
 	GPIO.output(pinNo, False)
 	pwm.ChangeDutyCycle(0)
 	
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pinNo,GPIO.OUT)
+
+pwm=GPIO.PWM(pinNo, 50)
+pwm.start(7.5)
+pwm.ChangeDutyCycle(7.5)
+sleep(5)
+pwm.ChangeDutyCycle(2.5)
+sleep(5)
+
+#SetAngle(90) 
+pwm.stop()
+GPIO.cleanup()
 
 	
